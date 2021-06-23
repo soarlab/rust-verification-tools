47f8c142f0eb99802a7bf0ab3d537d00483829c7
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2021-06-22 18:43:20 -0600

    Add smack to verification annotations

--- verification-annotations/build.rs
+++ verification-annotations/build.rs
@@ -10,10 +10,23 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     #[cfg(feature = "verifier-seahorn")]
     seahorn();
+    #[cfg(feature = "verifier-smack")]
+    smack();
 }
 
 #[cfg(feature = "verifier-seahorn")]
 fn seahorn() {
     println!("cargo:rerun-if-changed=lib/seahorn.c");
     cc::Build::new().file("lib/seahorn.c").compile("seahorn");
+}
+
+#[cfg(feature = "verifier-smack")]
+fn smack() {
+    println!("cargo:rerun-if-changed=lib/smack.c");
+    cc::Build::new()
+        .file("lib/smack.c")
+        .define("CARGO_BUILD", None)
+        .include("src")
+        .compiler("clang-10")
+        .compile("smack");
 }
